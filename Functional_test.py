d6e033edf8e5ae5e68f21163112378c05bcc23c6
jwsanabria <jwsanabria@gmail.com>
2016-10-15 23:00:24 -0500

    Paso 5 - Test para probar la edicion de los datos de un independiente (rojo)

--- Functional_test.py
+++ Functional_test.py
@@ -1,12 +1,13 @@
 from unittest import TestCase
 from selenium import webdriver
-from datetime import date
+from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as  EC
 
 class FunctionalTest(TestCase):
     def setUp(self):
+        self.random = datetime.today().strftime("%y%m%d_%I%M%p")
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
@@ -55,8 +56,6 @@ class FunctionalTest(TestCase):
 
         self.browser.implicitly_wait(5)
 
-        random = date.today().strftime("%y%m%d_%I%M%p")
-
         nombre = self.browser.find_element_by_id('id_nombre')
         nombre.send_keys('Diana')
 
@@ -74,7 +73,7 @@ class FunctionalTest(TestCase):
         correo.send_keys('dp.espitia@uniandes.edu.co')
 
         nombreUsuario = self.browser.find_element_by_id('id_username')
-        nombreUsuario.send_keys('dp.espitia.'+ random )
+        nombreUsuario.send_keys('dp.espitia.'+ self.random )
 
         imagen = self.browser.find_element_by_id('id_imagen')
         imagen.send_keys('C:\Users\William\Pictures\JuanaBoda.jpg')
@@ -89,6 +88,50 @@ class FunctionalTest(TestCase):
 
         self.assertIn('Diana Espitia', span.text)
 
+
+    def test_editar_perfil(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+
+        self.browser.implicitly_wait(3)
+
+        usuario = self.browser.find_element_by_id('login-username')
+        usuario.send_keys('dp.espitia')
+
+        password = self.browser.find_element_by_id('login-password')
+        password.send_keys('dp.espitia')
+
+        botonLogin = self.browser.find_element_by_id('id_entrar')
+        botonLogin.click()
+
+        self.browser.implicitly_wait(3)
+
+        link = self.browser.find_element_by_id('id_editar')
+        link.click()
+
+        self.browser.implicitly_wait(5)
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('7')
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollo Web']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys(self.random)
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('dp.espitia@uniandes.edu.co')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        imagen.send_keys('C:\Users\William\Pictures\JuanaBoda.jpg')
+
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+        self.browser.implicitly_wait(5)
+
+        link = self.browser.find_element_by_id('id_logout')
+
+        self.assertIsNotNone(link)
 
 
     def test_tittle(self):
